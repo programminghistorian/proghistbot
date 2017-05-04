0941257fdc0a07050d4f0a1461b6aab1515b4282
Brandon Walsh <bmw9t@virginia.edu>
2017-05-04 18:56:33 -0400

    Update Bot for tracking what has been tweeted.

--- bot.py
+++ bot.py
@@ -7,6 +7,11 @@ from oauth2client.service_account import ServiceAccountCredentials
 
 # Test on Production
 # Testing
+# rather than randomly choosing b/w lessons one and two you'll want
+# to say, look at column A. Find the X's. Now find the one X that is missing a
+# Y. tweet that message two.
+# Make sure it is still tweeting
+# When tweets are full it's not wiping out the one with the Y.
 # Test the tweets individually
 # Fill out all the data
 # Fill out the data with authors
@@ -50,14 +55,48 @@ def get_tweet_contents_from_google():
 
     gc = gspread.authorize(credentials)
     wks = gc.open_by_key('1o-C-3WwfcEYWipIFb112tkuM-XOI8pVVpA9_sag9Ph8')
-    list_of_items = wks.sheet1.get_all_values()[1:]
+    list_of_items = wks.sheet1.get_all_values()[1:5]
 
-    return list_of_items
+    return wks.sheet1, list_of_items
 
 
-def select_random_lesson(lessons):
+def update_sheet_queue_after_tweeting(sheet, id_num):
+    """After a tweet has been sent out, update the spreadsheet.
+    Add two to make the cells line up. Most recent tweet marked with
+    XY"""
+
+    cell_label = 'E' + str(int(id_num) + 2)
+    print(cell_label)
+    sheet.update_acell(cell_label, 'XY')
+
+
+def remove_last_tweet_marker(lessons, sheet):
+    """Remove the last tweet marker XY and replace with just X."""
+    for lesson in lessons:
+        if lesson[4].endswith('Y'):
+            sheet.update_acell('E' + str(int(lesson[0]) + 2), 'X')
+    pass
+
+
+def clear_queue(sheet, id_num):
+    cell_label = 'E' + str(int(id_num) + 2)
+    sheet.update_acell(cell_label, '')
+
+
+def select_random_lesson(lessons, sheet):
     """given a list of lessons, select one at random"""
-    return random.choice(lessons)
+
+    remaining_lessons = [
+        lesson for lesson in lessons if not lesson[4].startswith('X')]
+    if not remaining_lessons:
+        print('all out!')
+        for id_num in [lesson[0] for lesson in lessons]:
+            clear_queue(sheet, id_num)
+        the_choice = random.choice(lessons)
+    else:
+        print('some stuff remaining')
+        the_choice = random.choice(remaining_lessons)
+    return the_choice
 
 
 def select_random_message(lesson):
@@ -68,10 +107,14 @@ def select_random_message(lesson):
 
 def prepare_tweet():
     # options = get_tweet_contents_from_csv('tweet-manifest.csv')
-    options = get_tweet_contents_from_google()
-    lesson = select_random_lesson(options)
+    sheet, options = get_tweet_contents_from_google()
+    lesson = select_random_lesson(options, sheet)
     message = select_random_message(lesson)
     link = lesson[3]
+    # Something wonky going on here. get it to print out some variables of
+    # things you're tweeting. somehow it is marking things twice.
+    remove_last_tweet_marker(options, sheet)
+    update_sheet_queue_after_tweeting(sheet, lesson[0])
     return message + ' ' + link
 
 
