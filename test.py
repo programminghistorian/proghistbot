ba0ce04f7bc243d398710f2de242a58d847f32c3
Brandon Walsh <bmw9t@virginia.edu>
2017-05-06 16:36:59 -0400

    Testing out different credentials

---
+++ test.py
@@ -0,0 +1,17 @@
+import os
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+
+scope = ['https://spreadsheets.google.com/feeds']
+
+print(os.environ.get('json_credentials'))
+
+credentials = ServiceAccountCredentials.from_json(
+    os.environ.get('json_credentials'), scope)
+
+gc = gspread.authorize(credentials)
+wks = gc.open_by_key('1o-C-3WwfcEYWipIFb112tkuM-XOI8pVVpA9_sag9Ph8')
+list_of_items = wks.sheet1.get_all_values()[:5]
+headers = list_of_items.pop(0)
+
+print(wks.sheet1)
