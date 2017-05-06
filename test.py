c1a7d5a04db85c0da0d31fbf403ff3d5870270ad
Brandon Walsh <bmw9t@virginia.edu>
2017-05-06 16:40:39 -0400

    Updating for json credentials test

--- test.py
+++ test.py
@@ -6,9 +6,13 @@ scope = ['https://spreadsheets.google.com/feeds']
 
 print(os.environ.get('json_credentials'))
 
-credentials = ServiceAccountCredentials.from_json(
-    os.environ.get('json_credentials'), scope)
+# credentials = ServiceAccountCredentials.from_json(
+#     os.environ.get('json_credentials'), scope)
 
+credentials = ServiceAccountCredentials.from_json(
+    os.environ.get('json_credentials'))
+
+print(credentials)
 gc = gspread.authorize(credentials)
 wks = gc.open_by_key('1o-C-3WwfcEYWipIFb112tkuM-XOI8pVVpA9_sag9Ph8')
 list_of_items = wks.sheet1.get_all_values()[:5]
