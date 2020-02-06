5fd982ea72aabc0dbf22341e846ea3e6e691126c
walshbr <bmw9t@virginia.edu>
2020-02-06 09:47:20 -0500

    Fresh commit of master

---
+++ README.md
@@ -0,0 +1,93 @@
+# Read Me
+
+This repository collects the code used for the Twitter bot put together for [The Programming Historian](programminghistorian.org) by @walshbr.
+
+## Tweeting Process
+
+Data is served out of a [Google spreadsheet](https://docs.google.com/spreadsheets/d/1o-C-3WwfcEYWipIFb112tkuM-XOI8pVVpA9_sag9Ph8/edit#gid=1625380994).
+
+You'll want to only add data to the id, message_one, message_two, and link columns. The tweet_log is managed by the bot itself to track its progress through the corpus of lessons.
+
+Note: the ID column will probably be deprecated once I figure out a Pandas issue I was working through.
+
+As of May 2017, the tweets are scheduled to go out the following days/times each week:
+
+* Monday - 9AM PST, 12PM EST, 4PM London time
+* Thursday 6:30 AM PST, 9:30 AM EST, 1:30PM London
+
+The Thursday tweets are an ICYMI of Monday's content, reworded and often a bit more technical in nature. Each week the bot picks a random tweet from the spreadsheet. It will cycle through all the tweets until they're done, at which point it will start over again. So with one week per lesson we're looking at a year's worth of tweets.
+
+## Heroku
+
+From a more technical point of view, the bot is a Python script that runs certain days of the week on Heroku. The bot is called "proghistbot" on Heroku. As of right now, @walshbr owns the bot, and @mdlincoln, and @jerielizabeth are collaborators. If new people need to be added to the bot, contact @walshbr.
+
+Heroku only allows workers to run every 10 minutes, hourly, or daily. So we get around this by having two workers scheduled to each go off once a day. When they fire, they check to see what day it is. If it's not a day of the week when their particular worker is scheduled to go off, they pass. So we're filtering to only run Monday/Thursday. You can check the behaviors for these scheduled tasks by clicking on "Resources" from the dashboard for the proghistbot app when logged into Heroku. This resource is called "Heroku Scheduler." I've also included a task that allows you to run a tweet immediately for testing purposes. Under "Resources" from the app dashboard, the worker "python bot.py" is by default off. Turning this worker on by clicking the pencil at right will have the bot run a tweet immediately.
+
+Heroku logs can be viewed live from the terminal by running
+
+```
+$ heroku logs -t
+```
+
+Or, you can work locally by running:
+
+```
+$ heroku local
+```
+
+This will run the app locally using the procfile. In order to do so, however, you will need to store the Twitter credentials for the bot locally in a .env file. I've provided a template file, "template.env", that you can rename to ".env" - just place the bot credentials with the quotation marks. All Twitter credentials are stored as config variables in Heroku. You access them from the dashboard of the proghistbot app on Heroku by clicking on "Settings" > "Config Vars". These are linked to the Programming Historian's Twitter account, so they shouldn't ever need to be changed.
+
+## Deploying and Development
+
+To deploy this yourself, you'll need a few things, depending on what you're trying to do:
+
+* Write access to the Google spreadsheet.
+* Write access to the Heroku remote, which can be granted by email.
+* A JSON key that enables us to use the Google spreadsheet API. This can be shared by email.
+
+It's important that we not commit sensitive data to the GitHub repository. The Twitter Credentials are all held in Environment variables in Heroku, but the Google spreadsheet JSON key is a bit more wonky. I've set it up so that the master branch points to GitHub while a separate branch, heroku, points to the heroku remote that deploys the remote. The heroku branch will never be committed to GitHub. The JSON key will never be committed to the master branch, so any future work will be to the master branch. When we merge into the heroku branch, our changes will be added without overwriting the heroku branch.  
+
+For that reason, the general workflow to modify the bot is this. First clone the bot down and make your heroku branch:
+
+```bash
+$ git clone git@github.com:walshbr/proghistbot.git
+$ git branch heroku
+$ git checkout heroku
+```
+
+Then, add the JSON key (should be shared by email) to the repository and push it up to Heroku remote.
+
+```bash
+$ git add 'proghist-google-credentials.json'
+$ git add 'Setting up access to the Heroku remote.'
+$ git push heroku heroku:master
+```
+
+From then on out you'll work on the master branch, merge into heroku, and push up your changes to the heroku branch.
+
+```bash
+$ git checkout master
+[edit some files]
+$ git add .
+$ git commit -m 'Share a helpful message.'
+$ git push origin master.
+```
+
+When ready to deploy to the heroku remote itself, you'll merge into the heroku branch.
+
+```bash
+$ git checkout heroku
+$ git merge master
+$ git push heroku heroku:master
+```
+
+When in doubt, you can always run
+```bash
+$ heroku run bash
+```
+
+to make sure that you haven't accidentally deleted the JSON key from the Heroku remote.
+
+## Generating the JSON Keychain
+
+In the event that we need to generate a fresh copy of the JSON key for whatever reason, you can do so from from the [Google Cloud Console](https://console.cloud.google.com/). After logging in, select the "Programming Historian" project, which might be variously referred to by its autogenerated name - rare-chiller-166520. To generate the Keychain, follow the steps documented by the [gspread library that we use for tweeting](http://gspread.readthedocs.io/en/latest/oauth2.html). You'll need to change the filename to 'proghist-google-credentials.json' and then follow the steps above.
---
+++ bot.py
@@ -0,0 +1,228 @@
+from __future__ import unicode_literals
+import os
+import sys
+import argparse
+from tweepy import OAuthHandler, API
+import random
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+import pandas as pd
+import traceback
+import time
+
+# TODO: Refactor for better error handling
+# TODO: Actually select the days you want and get heroku running accordingly.
+
+# For local testing you just run the script with regular arguments based on what language you're testing:
+# $ python bot.py -es True -t True
+# Heroku will add separate arguments for the scheduling, but this should immediately send a tweet out from the ProgHist account (or offer the opportunity to debug).
+
+ACCESS_TOKEN = os.environ.get('ACCESS_TOKEN')
+ACCESS_TOKEN_SECRET = os.environ.get('ACCESS_TOKEN_SECRET')
+CONSUMER_KEY = os.environ.get('CONSUMER_KEY')
+CONSUMER_SECRET = os.environ.get('CONSUMER_SECRET')
+
+
+def rest(max_sleep):
+    """Rest after tweeting - used when testing. """
+    time.sleep(random.random() * max_sleep)
+
+
+def twitter_api():
+    """Initialize Twitter API, authorize, and return a usable object."""
+    access_token = ACCESS_TOKEN
+    access_token_secret = ACCESS_TOKEN_SECRET
+    consumer_key = CONSUMER_KEY
+    consumer_secret = CONSUMER_SECRET
+
+    auth = OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+    api = API(auth)
+    return api
+
+
+def tweet(message):
+    """Tweets a prepared message using the authorized API."""
+    print(message)
+    api = twitter_api()
+    api.update_status(status=message)
+
+
+def get_tweet_contents_from_google(spanish=False, french=False):
+    """Reads in contents of google spreadsheet. Returns both the
+    spreadshet for updating and a Pandas dataframe of the data
+    for working with."""
+    scope = ['https://spreadsheets.google.com/feeds']
+
+    credentials = ServiceAccountCredentials.from_json_keyfile_name(
+        'proghist-google-credentials.json', scope)
+
+    gc = gspread.authorize(credentials)
+    wks = gc.open_by_key('1o-C-3WwfcEYWipIFb112tkuM-XOI8pVVpA9_sag9Ph8')
+    if spanish:
+        print('tweeting in spanish')
+        list_of_items = wks.worksheet('Spanish').get_all_values()
+        headers = list_of_items.pop(0)
+        return wks.worksheet('Spanish'), pd.DataFrame(list_of_items, columns=headers)
+    if french:
+        print('tweeting in french')
+        list_of_items = wks.worksheet('French').get_all_values()
+        headers = list_of_items.pop(0)
+        return wks.worksheet('French'), pd.DataFrame(list_of_items, columns=headers)
+    else:
+        print('tweeting in english')
+        list_of_items = wks.sheet1.get_all_values()
+        headers = list_of_items.pop(0)
+        return wks.sheet1, pd.DataFrame(list_of_items, columns=headers)
+
+
+def update_sheet_queue_after_tweeting(sheet, id_num):
+    """After a tweet has been sent out, update the spreadsheet.
+    Add two to make the cells line up with the indexing of the data.
+    Most recent tweet is marked with XY, so as to distinguish it from
+    the rest of the previous tweets that are marked with X."""
+    
+    cell_label = 'D' + str(int(id_num.values[0]) + 2)
+    print('========')
+    print('update_sheet_queue')
+    print(cell_label)
+    print('========')
+    sheet.update_acell(cell_label, 'XY')
+
+
+def remove_last_tweet_marker(lessons_frame, sheet):
+    """Remove the tweet marker XY from the previous tweet and
+    replace with just X. Also catches a couple bugs - will remove multiple
+    XY markers if they exist (though I fixed that problem), and will log if
+    there don't appear to be any last tweet markers."""
+    last_tweet = lessons_frame.tweet_log.str.endswith('Y')
+    print('looking for last markers')
+    print(lessons_frame[last_tweet])
+    if lessons_frame[last_tweet].index.any() or \
+            lessons_frame[last_tweet].index.values[0] == 0:
+        print('found one')
+        for index_num in lessons_frame[last_tweet].index.values:
+            cell_label = 'D' + str(index_num + 2)
+            print('=====')
+            print('remove_last_tweet_marker')
+            print(cell_label)
+            print('=====')
+            sheet.update_acell(cell_label, 'X')
+    else:
+        print('could not find any last tweet markers')
+        print(lessons_frame[last_tweet].index)
+        pass
+
+
+def clear_queue(sheet, id_num):
+    """Cleans out the Tweet log when everything has been tweeted so that
+    the tweet queue will restart."""
+    cell_label = 'D' + str(int(id_num) + 2)
+    print('======')
+    print('cleaning_queue')
+    print(cell_label)
+    print('======')
+    sheet.update_acell(cell_label, '')
+
+
+def select_random_lesson(lessons_frame, sheet):
+    """Given a list of lessons, select one at random from those that haven't
+    been tweeted. If there are none left, clear the queue and pick a random one
+    from the new set."""
+    remaining_lessons = ~lessons_frame.tweet_log.str.startswith('X')
+    if remaining_lessons.any():
+        print('some stuff remaining')
+        the_choice = lessons_frame.iloc[[random.choice(
+            lessons_frame[remaining_lessons]
+            .index.values)]]
+    else:
+        print('all out!')
+        for id_num in lessons_frame.index.values:
+            clear_queue(sheet, id_num)
+        the_choice = lessons_frame.iloc[[random.choice(
+            lessons_frame.index.values)]]
+    return the_choice
+
+
+def select_first_message(lesson):
+    """Select the first message of a given lesson."""
+    return lesson.message_one.values[0]
+
+
+def select_second_message(lesson):
+    """Select the second message of a given lesson."""
+    return lesson.message_two.values[0]
+
+
+def prepare_tweet(day_two=False, spanish=False, french=False):
+    """Prepare the tweet for tweeting. Workflow:
+    * Get the google spreadsheet and dataframe from an authorized account.
+    * Remove the last tweet's marker.
+    * If this is the second day of the week, look for the previous tweet
+        and send out the second message for that lesson.
+    * If not, it's the first day of the week. So select a lesson at random
+        and its associated link.
+    * Update the queue based on what is about to be tweeted.
+    * Return a string for the tweet consisting of the message and the link.
+    """
+    sheet, options_frame = get_tweet_contents_from_google(spanish, french)
+    remove_last_tweet_marker(options_frame, sheet)
+    if day_two:
+        last_tweet = options_frame.tweet_log.str.endswith('Y')
+        lesson = options_frame[last_tweet]
+        message = select_second_message(lesson)
+    else:
+        lesson = select_random_lesson(options_frame, sheet)
+        message = select_first_message(lesson)
+    link = lesson.link.values[0]
+    update_sheet_queue_after_tweeting(sheet, lesson.index)
+
+    return message + ' ' + link
+
+
+def parse_args(argv=None):
+    """Parses the command line for arguments.
+    Two different potential arguments. One indicates if it is the
+    second tweet of the day (assumes that is not the case). Another marks
+    whether we should be tweeting Spanish (not implemented yet).
+    """
+    argv = sys.argv[1:] if argv is None else argv
+    parser = argparse.ArgumentParser(description=__doc__)
+
+    parser.add_argument('-t', '--two', dest='day_two', action='store',
+                        default=False,
+                        help='Set tweets for second message')
+    parser.add_argument('-es', '--spanish', dest='spanish', type=bool,
+                        default=False,
+                        help='Set tweets to be Spanish. '
+                        'Default = False')
+    parser.add_argument('-fr', '--french', dest='french', type=bool,
+                        default=False,
+                        help='Set tweets to be French. '
+                        'Default = False')    
+    return parser.parse_args(argv)
+
+def main():
+    """Tweet a lesson."""
+
+    # Get the arguments and prepare the tweet contents.
+    args = parse_args()
+    print('grabbing tweet contents')
+    tweet_contents = prepare_tweet(args.day_two, args.spanish, args.french)
+    print('tweet contents grabbed')
+    # Try to tweet. If it works, log the success. If it doesn't, log the stack
+    # trace for debugging later.
+    try:
+        print('trying to tweet')
+        tweet(tweet_contents)
+        print('Success: ' + tweet_contents)
+    except:
+        # catch in the logs on heroku
+        print('Something went wrong')
+        print('Fail: ' + tweet_contents)
+        print(traceback.format_exc())
+    rest(600)
+
+
+if __name__ == '__main__':
+    main()
